25eb8d26409f381a0dddff47e1c7bacc416f7b4e
Corne <39339251+corne12345@users.noreply.github.com>
2018-11-20 14:59:36 +0100

    minor fixes

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -1,5 +1,8 @@
 import csv
 
-with open("wind.txt") as txtfile:
-    file = csv.reader(txtfile, delimiter=',')
-    print(file)
+f = open("wind.txt", "r")
+print(f)
+
+# with open("wind.txt", newline='') as txtfile:
+#     file = csv.reader(txtfile, delimiter=',')
+#     print(file)
