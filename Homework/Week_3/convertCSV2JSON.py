6ebcfc93d9028eb0681ca7eb005401bb03e5f7dc
Corne <39339251+corne12345@users.noreply.github.com>
2018-11-20 16:56:30 +0100

    data to json

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -1,8 +1,24 @@
 import csv
+import json
 
-f = open("wind.txt", "r")
-print(f)
 
-# with open("wind.txt", newline='') as txtfile:
-#     file = csv.reader(txtfile, delimiter=',')
-#     print(file)
+vlieland = []
+vlissingen = []
+maastricht = []
+with open("wind.csv", newline='') as csvfile:
+    file = csv.reader(csvfile, delimiter=',')
+    for row in file:
+        if len(row) == 3:
+            row[0] = row[0].lstrip()
+            row[2] = row[2].lstrip()
+            if row[0] == "242":
+                vlieland.append(row[1:])
+            elif row[0] == "310":
+                vlissingen.append(row[1:])
+            elif row[0] == "380":
+                maastricht.append(row[1:])
+
+print(maastricht)
+rows = [vlieland, vlissingen, maastricht]
+with open("test.json", "w") as f:
+    json.dump(rows,f)
